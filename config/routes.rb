cdbad97fe689cb23fac7e28fa15f210bae749141
klementtan <klementtan@gmail.com>
2019-12-12 17:47:12 +0800

    Basic Authentication API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      post 'users/create'
+      get 'users/show'
+      delete 'users/destroy'
+
+      post '/login' => 'sessions#create'
+      delete '/logout' => 'sessions#destroy'
+      get '/logged_in' => 'sessions#is_logged_in?'
+    end
+  end
 end

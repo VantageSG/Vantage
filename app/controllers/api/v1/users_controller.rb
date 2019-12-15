2ec967bd2fb6683b5b3e512b0002e3633f68f67e
klementtan <klementtan@gmail.com>
2019-12-16 05:08:52 +0800

    Resolve hound 2

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
   private
   def user_params
     params
-    .require(:user)
-    .permit(:username, :email, :password, :password_confirmation)
+      .require(:user)
+      .permit(:username, :email, :password, :password_confirmation)
   end
 end

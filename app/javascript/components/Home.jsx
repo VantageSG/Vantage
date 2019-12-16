3c62a8fb17c23d3265c876da24fac8afac5c322b
eugene teu <eugeneteu@gmail.com>
2019-12-17 00:59:04 +0800

    added landing container

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Image,
-  Message,
-  Segment
-} from "semantic-ui-react";
-import Login from "./registrations/Login";
+
 import Landing from "./Landing";
 const Home = () => {
   return (
--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -1,72 +1,168 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import {
   Button,
-  Form,
+  Container,
+  Divider,
   Grid,
   Header,
+  Icon,
   Image,
-  Message,
+  List,
+  Menu,
+  Responsive,
   Segment,
-  Icon
+  Sidebar,
+  Visibility
 } from "semantic-ui-react";
 import Login from "./registrations/Login";
 
-const Landing = () => {
-  return (
-    <div>
-      <Grid
-        textAlign="center"
-        style={{ height: "80vh" }}
-        verticalAlign="middle"
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Icon loading name="home" size="massive" />
+const getWidth = () => {
+  const isSSR = typeof window === "undefined";
 
-          <Header as="h1" color="teal" textAlign="center">
-            Welcome to Vantage
-          </Header>
-          <Header as="h3" color="teal" textAlign="center">
-            An Initiative brought to you by DSC NUS
-          </Header>
-
-          <Segment stacked>
-            <Button
-              color="green"
-              fluid
-              size="large"
-              as={Link}
-              to="/login"
-              icon="sign in alternate"
-              content="Login"
-            ></Button>
-
-            <br></br>
-
-            <Button
-              color="blue"
-              fluid
-              size="large"
-              as={Link}
-              to="/signup"
-              icon="registered"
-              content="Sign up"
-            ></Button>
-            <br></br>
-
-            <Button
-              color="red"
-              fluid
-              size="large"
-              as={Link}
-              to="/aboutus"
-              icon="street view"
-              content="About Us"
-            ></Button>
-          </Segment>
-        </Grid.Column>
-      </Grid>
-    </div>
-  );
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
 };
+
+class Landing extends Component {
+  state = {};
+
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
+
+  render() {
+    const { children } = this.props;
+    const { fixed } = this.state;
+
+    return (
+      <div>
+        <Responsive
+          getWidth={getWidth}
+          minWidth={Responsive.onlyTablet.minWidth}
+        >
+          <Visibility
+            once={false}
+            onBottomPassed={this.showFixedMenu}
+            onBottomPassedReverse={this.hideFixedMenu}
+          >
+            <Segment
+              inverted
+              textAlign="center"
+              style={{ minHeight: 700, padding: "1em 0em" }}
+              vertical
+            >
+              <Menu
+                fixed={fixed ? "top" : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size="large"
+              >
+                <Container>
+                  <Menu.Item as="a" active>
+                    Home
+                  </Menu.Item>
+                  <Menu.Item as="a">Work</Menu.Item>
+                  <Menu.Item as="a">Company</Menu.Item>
+                  <Menu.Item as="a">Careers</Menu.Item>
+                  <Menu.Item position="right">
+                    {/*
+                    <Button as="a" inverted={!fixed}>
+                      Log in
+                    </Button>
+                    <Button
+                      as="a"
+                      inverted={!fixed}
+                      primary={fixed}
+                      style={{ marginLeft: "0.5em" }}
+                    >
+                      Sign Up
+                    </Button>*/}
+                  </Menu.Item>
+                </Container>
+              </Menu>
+              <TitleCard />
+            </Segment>
+          </Visibility>
+
+          {children}
+        </Responsive>
+      </div>
+    );
+  }
+}
+const TitleCard = ({ mobile }) => (
+  <Container text>
+    <Icon name="home" size="huge" />
+    <Header
+      as="h1"
+      content="Welcome to Vantage"
+      inverted
+      style={{
+        fontSize: mobile ? "2em" : "4em",
+        fontWeight: "normal",
+        marginBottom: 0,
+        marginTop: "0em"
+      }}
+    />
+    <Header
+      as="h2"
+      content="Break your boundaries"
+      inverted
+      style={{
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0em" : "0em"
+      }}
+    />
+    <Header
+      as="h3"
+      textAlign="center"
+      content=" An Initiative brought to you by DSC NUS"
+      inverted
+      style={{
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em"
+      }}
+    ></Header>
+    <Grid textAlign="center" verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Segment stacked inverted>
+          <Button
+            color="green"
+            fluid
+            size="large"
+            as={Link}
+            to="/login"
+            icon="sign in alternate"
+            content="Login"
+          ></Button>
+
+          <br></br>
+
+          <Button
+            color="blue"
+            fluid
+            size="large"
+            as={Link}
+            to="/signup"
+            icon="registered"
+            content="Sign up"
+          ></Button>
+          <br></br>
+
+          <Button
+            color="red"
+            fluid
+            size="large"
+            as={Link}
+            to="/aboutus"
+            icon="street view"
+            content="About Us"
+          ></Button>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  </Container>
+);
 export default Landing;

522531a6a21b6ab8503d3e3ce60e859e4b09fd3f
peirong.wu <wupeirong294@gmail.com>
2019-12-25 11:37:09 +0800

    rename cvurl attribute to cv

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,7 +2,4 @@ class UserProfile < ApplicationRecord
     belongs_to :user
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
-    validates_uniqueness_of :email
-
 end

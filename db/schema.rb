ff7b43c314cf74fa4b349b4e0a7725b7c7055618
peirong.wu <wupeirong294@gmail.com>
2019-12-25 11:23:23 +0800

    remove email from UserProfile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_083450) do
+ActiveRecord::Schema.define(version: 2019_12_25_021505) do
 
   create_table "user_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "cvurl"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"

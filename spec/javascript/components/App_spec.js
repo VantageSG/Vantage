672c906b4f41b7be8fe51f677e52dd6e0c66e107
klementtan <klementtan@gmail.com>
2019-12-28 16:11:34 +0800

    Resolve hound:

--- spec/javascript/components/App_spec.js
+++ spec/javascript/components/App_spec.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import App from 'components/App.jsx'
+import React from 'react';
+import App from 'components/App.jsx';
 import { render, screen } from 'test-utils';
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
 
 test('Should not be signed in initially', () => {
   render(<App></App>);
--- spec/javascript/test-utils.js
+++ spec/javascript/test-utils.js
@@ -1,11 +1,11 @@
-import { render } from '@testing-library/react'
+import { render } from '@testing-library/react';
 
 function customRender(ui) {
   render(ui);
 }
 
 // re-export everything
-export * from '@testing-library/react'
+export * from '@testing-library/react';
 
 // override render method
-export { customRender as render }
+export { customRender as render };

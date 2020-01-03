f5ed829cbeb2ee87682a967157d82ac58789ee73
klementtan <klementtan@gmail.com>
2020-01-03 18:36:44 +0800

    Resolve hound 2

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe Api::V1::SessionsController, type: :request do
     }
   end
   describe 'POST /api/v1/login' do
-
     context 'successful logged in' do
-
-      before {post '/api/v1/login', params: typical_user_jason_credentials}
+      before do
+       post '/api/v1/login', params: typical_user_jason_credentials
+      end
 
       it 'should be logged in' do
         expect(json['logged_in']).to eq(true)
@@ -25,36 +25,31 @@ RSpec.describe Api::V1::SessionsController, type: :request do
         expected_user_id = User.find_by(email: 'JasonYip@gmail.com').id
         expect(json['user']['id']).to eq(expected_user_id)
       end
-
     end
 
     context 'invalid logged in' do
-
-      before {
+      before do
         invalid_credentials = typical_user_jason_credentials.clone
         invalid_credentials[:user][:password] = 'foobar'
         post '/api/v1/login', params: invalid_credentials
-      }
+      end
 
-      it 'should be logged in' do  
+      it 'should be logged in' do
         expect(response.status).to eq(401)
       end
 
       it 'should return error message' do
-        expect(json['errors']).to eq('verify credentials and try again or signup')
+        expect(json['errors']).to 
+          eq('verify credentials and try again or signup')
       end
-
     end
-
   end
 
   describe 'GET /api/v1/logged_in' do
-
     context 'user logged in' do
-
-      before {
+      before do
         post '/api/v1/login', params: typical_user_jason_credentials
-      }
+      end
 
       it 'should return user is logged_in' do
         request.cookies['_Vantage'] = response.cookies['_Vantage']
@@ -65,7 +60,8 @@ RSpec.describe Api::V1::SessionsController, type: :request do
       it 'should return valid user is credentials' do
         request.cookies['_Vantage'] = response.cookies['_Vantage']
         get '/api/v1/logged_in'
-        expect(json['user']['id']).to eq(User.find_by(email: 'JasonYip@gmail.com').id)
+        expect(json['user']['id']).to 
+        eq(User.find_by(email: 'JasonYip@gmail.com').id)
         expect(json['user']['email']).to eq('JasonYip@gmail.com')
         expect(json['user']['username']).to eq('JasonYip')
       end

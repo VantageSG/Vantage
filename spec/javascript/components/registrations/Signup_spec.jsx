05122e32ea505d78d1db42e7ac0c93e15d8e3ae4
klementtan <klementtan@gmail.com>
2020-01-05 14:49:24 +0800

    Test Signup halfway

--- spec/javascript/components/registrations/Signup_spec.jsx
+++ spec/javascript/components/registrations/Signup_spec.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
 import Signup from 'components/registrations/Signup.jsx';
-import { render, screen } from 'test-utils';
+import { screen } from 'test-utils';
 import '@testing-library/jest-dom/extend-expect';
-import {createMemoryHistory} from 'history'
+import axiosMock from 'axios'
 import { fireEvent, getByText } from '@testing-library/dom';
-
-function renderWithRouter(
-  ui,
-  {route = '/Signup', history = createMemoryHistory({initialEntries: [route]})} = {},
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
-  }
-}
+import { renderWithRouter } from '../../test-utils';
 
 describe('Login page', () => {
-  renderWithRouter(<Signup/>);
+  jest.mock('axios')
+  renderWithRouter(<Signup/>)
   it('should have email form', () => {
     expect(screen.getByPlaceholderText("Email")).toBeInTheDocument();
   });
@@ -30,16 +18,6 @@ describe('Login page', () => {
   it('should have password confirmation', () => {
     expect(screen.getByPlaceholderText("password confirmation")).toBeInTheDocument();
   });
-  // it('should have redirect to home page', () => {
-  //   const redirectUrl = '/'
-  //   const button = screen.getByText('home');
-  //   expect(button).not.toBe(null);
-  //   fireEvent.click(button);
-
-  //   expect(screen.innerHTML).toEqual(
-  //     expect.stringContaining(redirectUrl)
-  //   )
-  // });
 })
 
 

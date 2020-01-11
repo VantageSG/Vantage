faa16de7cce308c707fbdf0f98c63662ed238748
klementtan <klementtan@gmail.com>
2020-01-11 16:58:08 +0800

    Resolve login page redirect bug

--- app/javascript/components/registrations/Login.jsx
+++ app/javascript/components/registrations/Login.jsx
@@ -23,7 +23,7 @@ class Login extends Component {
       errors: ""
     };
   }
-  componentDidMount() {
+  componentDidUpdate() {
     return this.props.loggedInStatus ? this.redirect() : null;
   }
   handleChange = (event) => {

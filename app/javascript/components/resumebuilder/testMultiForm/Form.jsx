3170d0b9321399c79aaf698b7e582e0197a60c9c
klementtan <klementtan@gmail.com>
2020-01-20 13:57:58 +0800

    Remove unused code

--- app/javascript/components/resumebuilder/testMultiForm/Form.jsx
+++ app/javascript/components/resumebuilder/testMultiForm/Form.jsx
@@ -16,60 +16,6 @@ import Skills from "./formPages/Skills";
 import Interests from "./formPages/Interests";
 import { Animated } from "react-animated-css";
 
-// const FormActionButtons = props => {
-//   return (
-//     <div className="ui two buttons">
-//       <Button
-//         color="red"
-//         onClick={props.previousStep}
-//         content="previous"
-//       ></Button>
-//       <Button.Or />
-
-//       {props.step === props.maxStep ? (
-//         <ModalConfirm
-//           props={props}
-//           color="green"
-//           onClick={props.submitAndContinue}
-//           content="Confirm and Submit"
-//         ></ModalConfirm>
-//       ) : (
-//         <Button color="orange" onClick={props.nextStep} content="next"></Button>
-//       )}
-//     </div>
-//   );
-// };
-
-const ModalConfirm = props => {
-  console.log(props);
-  return (
-    <Modal
-      trigger={
-        <Button
-          color="green"
-          onClick={props.submitAndContinue}
-          content="Confirm and Submit"
-        ></Button>
-      }
-      centered={false}
-    >
-      <Modal.Header>Please Confirm your details!</Modal.Header>
-      <Modal.Content image>
-        <Image
-          wrapped
-          size="medium"
-          src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
-        />
-        <Modal.Description>
-          <Header>The following are your details!</Header>
-
-          <p>Is it okay?</p>
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  );
-};
-
 export default class FormStep extends Component {
   constructor(props) {
     super(props);
@@ -189,7 +135,7 @@ export default class FormStep extends Component {
       end,
       achievements
     };
-    
+
     {
       switch (step) {
         case 0:

6e5710e5e288d4bb5947caa74913b274bdd159b5
klementtan <klementtan@gmail.com>
2020-01-27 01:16:18 +0800

    Test 1

--- spec/javascript/components/resumebuilder/stepform/formPages/ConfirmationPage_spec.jsx
+++ spec/javascript/components/resumebuilder/stepform/formPages/ConfirmationPage_spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ConfirmationPage from 'components/resumebuilder/multiStepform/formPages/ConfirmationPage.jsx';
+import ConfirmationPage from 'components/resumebuilder/multiStepForm/formPages/ConfirmationPage.jsx';
 import { renderWithRouter, screen } from 'test-utils';
 import '@testing-library/jest-dom/extend-expect';
 

f61d7d6031989cde5bddc08c086fd9384d399778
shermzlim <shermz.lim.jh@gmail.com>
2020-01-27 23:19:25 +0800

    Added test for resume api. changed case_sensitive for emails uniqueness check to false

--- app/models/about.rb
+++ app/models/about.rb
@@ -2,7 +2,7 @@ class About < ApplicationRecord
   belongs_to :resume
   validates :name, presence: true
   validates :email, presence: true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :contact_number, presence: true
   validates :about_me, presence: true
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -2,6 +2,6 @@ class Referee < ApplicationRecord
   belongs_to :work_experience
   validates :name, presence: true
   validates :email, presence: true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end

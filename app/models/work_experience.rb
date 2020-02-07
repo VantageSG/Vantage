d37b6a5b87e22204f8cb4f268bafad19832c9a3d
shermzlim <shermz.lim.jh@gmail.com>
2020-02-07 19:56:20 +0800

    Uncommented out model validations

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -2,12 +2,12 @@ class WorkExperience < ApplicationRecord
   belongs_to :resume
   has_many :referee, :dependent => :delete_all
 
-  # validates :title, presence: true
-  # validates :company, presence: true
-  # validates :start, presence: true
-  # validates :start, numericality: { only_integer: true }
-  # validates :end, presence: true
-  # validates :end, numericality: { only_integer: true }
-  # validates :achievements, presence: true
+  validates :title, presence: true
+  validates :company, presence: true
+  validates :start, presence: true
+  validates :start, numericality: { only_integer: true }
+  validates :end, presence: true
+  validates :end, numericality: { only_integer: true }
+  validates :achievements, presence: true
 
 end

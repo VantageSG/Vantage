7bfe648746ccd529f2e94711137f4f263682329d
klementtan <klementtan@gmail.com>
2020-02-07 20:20:25 +0800

    Resolve test case

--- spec/javascript/components/resumebuilder/stepform/formPages/ConfirmationPage_spec.jsx
+++ spec/javascript/components/resumebuilder/stepform/formPages/ConfirmationPage_spec.jsx
@@ -4,9 +4,11 @@ import { renderWithRouter, screen } from 'test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Default confirmation page', () => {
-  renderWithRouter(<ConfirmationPage/>);
+  renderWithRouter(<ConfirmationPage
+    user={{id:1}}
+  />);
   it('should have header', () => {
-    expect(screen.getByText(/Confirmation page/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Confirmation page/i).length).toBe(2);
   });
   
 })

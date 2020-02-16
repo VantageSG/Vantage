38d655c81a5711899f37e5d60026593c80b63f09
peirong.wu <wupeirong294@gmail.com>
2020-02-16 18:34:56 +0800

    Add Grammar formatting for About Me and Work Experience

--- app/controllers/api/v1/resumes/abouts_controller.rb
+++ app/controllers/api/v1/resumes/abouts_controller.rb
@@ -1,3 +1,5 @@
+require 'gingerice'
+
 class Api::V1::Resumes::AboutsController < Api::V1::Resumes::BaseVrsController
   def get_component_format
     {about: @resume.about}
@@ -10,6 +12,12 @@ class Api::V1::Resumes::AboutsController < Api::V1::Resumes::BaseVrsController
   end
 
   def update_component
+    # Grammar formatting for about_me before posting to DB
+    text = params[:about][:about_me]
+
+    parser = Gingerice::Parser.new
+    result = parser.parse text
+    params[:about][:about_me] = result['result']
     @resume.build_about(params.require(:about).permit(:name, :email, :contact_number, :about_me)).save!
   end
 end

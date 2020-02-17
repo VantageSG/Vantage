2679b4d8bbd926646195497eb061d37967dc1ccf
klementtan <klementtan@gmail.com>
2020-02-18 04:10:42 +0800

    working prod env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins '*'
+    origins 'http://localhost:3000, http://vantage-sg.com'
     resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end

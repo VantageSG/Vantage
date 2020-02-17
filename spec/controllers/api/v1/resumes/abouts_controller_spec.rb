c9f6737010118532ffc420ec0b7b1ef4e4d35807
peirong.wu <wupeirong294@gmail.com>
2020-02-17 23:06:19 +0800

    Fix snakecase in abouts_controller_spec v2

--- spec/controllers/api/v1/resumes/abouts_controller_spec.rb
+++ spec/controllers/api/v1/resumes/abouts_controller_spec.rb
@@ -52,8 +52,8 @@ RSpec.describe Api::V1::Resumes::AboutsController, type: :request do
         "about": {
           "name": "USER'S FULL NAME: str",
           "email": "notanemail",
-          "contact_Number": "123456789",
-          "about_Me": "SHORT WRITE UP OF USER: str"
+          "contact_number": "123456789",
+          "about_me": "SHORT WRITE UP OF USER: str"
         }
       }
     end

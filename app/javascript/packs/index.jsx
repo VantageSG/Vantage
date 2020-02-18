404f4e02df91f6471468ab71a123de5864de5e4b
klementtan <klementtan@gmail.com>
2020-02-18 21:25:22 +0800

    loaded .less but styling not applied

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,7 +4,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../components/App";
-import "../../assets/dist/semantic.min.css";
+
+require("semantic-ui-less/semantic.less");
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,39 @@
 const { environment } = require('@rails/webpacker')
+const path = require('path');
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
+
+const config= {
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+          "../../theme.config$": path.join(__dirname, "../../app/assets/src/theme.config"),
+          "../semantic-ui/site": path.join(__dirname, "../../app/assets/src/site")
+        }
+    },
+    module: {
+        //CSS, SASS
+        rules: [
+          {
+            test: /\.(less)$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "less-loader"
+            ]
+        }
+        ]
+    },
+    plugins: [
+        // this handles the bundled .css output file
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+            //filename: "./css/[name].css"
+        })
+    ]
+};
+
+environment.config.merge(config)
 
 module.exports = environment
